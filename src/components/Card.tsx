1bf0c9092d56d2e5dccd777ac6d8488b82c9cd7b
xcedeno <xcedeno281@gmail.com>
2025-01-13 20:09:13 -0400

    agregando interfaces y configuraciones para administrador

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -4,14 +4,16 @@ import './card.css';
 interface CardProps {
 number: number;
 isSelected: boolean;
+isSelecting: boolean;
 onSelect: (number: number) => void;
+children?: React.ReactNode;
 }
 
-const Card: React.FC<CardProps> = ({ number, isSelected, onSelect }) => {
+const Card: React.FC<CardProps> = ({ number, isSelected, isSelecting, onSelect }) => {
 return (
 <div
     onClick={() => !isSelected && onSelect(number)}
-    className={`card ${isSelected ? 'selected' : 'not-selected'}`}
+    className={`card ${isSelected ? 'selected' : isSelecting ? 'selecting' : 'not-selected'}`}
 >
     {number}
 </div>
